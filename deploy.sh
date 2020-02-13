164af24907a4ba5a26f247b1d883f61ce2cf0fe1
Donald Nevtis <nevtees@gmail.com>
2020-02-13 09:06:58 +0300

    link fix

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:donnevtis/donnevtis.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:donnevtis/ymap.git master:gh-pages
+git push -f git@github.com:donnevtis/ymap-component.git master:gh-pages
 
 cd -
